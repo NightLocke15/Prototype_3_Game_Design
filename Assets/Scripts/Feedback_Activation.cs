cb45a6698628385c0af14c360058bfe8aa9b7b51
2615283 <131881245+NightLocke15@users.noreply.github.com>
2024-03-18 22:17:05 +0200

    Lots of changes

---
+++ Assets/Scripts/Feedback_Activation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Feedback_Activation : MonoBehaviour
+{
+    #region Booleans
+    public bool soundOn;
+    #endregion
+
+    #region Variables
+    public Toggle footstep;
+    #endregion
+
+    #region Effects
+    public AudioSource Footsteps;
+    public TextMeshProUGUI footstepActivationTXT;
+    #endregion
+
+    private void Update()
+    {
+        if (footstep.isOn)
+        {
+            Footsteps.enabled = true;
+            footstepActivationTXT.text = "Active";
+            soundOn = true;
+        }
+        else
+        {
+            Footsteps.enabled = false;
+            footstepActivationTXT.text = "Not Active";
+            soundOn = false;
+        }
+    }
+}
--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
+    Feedback_Activation feedbackActivation;
+
     #region VARIABLES
     [SerializeField]private float playerSpeed;
     [SerializeField] private float playerJumpSpeed;
     public bool onFloor = false;
+    public bool isMoving;
+    private float TimeFootstep = 0.2f;
+    private float timeBetweenFootsteps;
+    private KeyCode LastKeyPress;
+    [SerializeField] private float timeUp;
     #endregion
 
     #region GAMEOBJECTS & ITS STUFF
     private GameObject player;
     private Rigidbody2D playerRB;
     private GameObject gun;
+    [SerializeField] AudioSource footsteps;
+    [SerializeField] AudioClip footstep;
+    [SerializeField] Animator PlayerAnimator;
+    [SerializeField] ParticleSystem dustParticles;
+    [SerializeField] ParticleSystem dustParticlesLand;
     #endregion
 
     private void Start()
     {
         player = GameObject.Find("Player");
         playerRB = GameObject.Find("Player").GetComponent<Rigidbody2D>();
-        gun = GameObject.Find("Gun Holder");
+        gun = GameObject.Find("Gun");
+        feedbackActivation = GameObject.Find("UI").GetComponent<Feedback_Activation>();
     }
 
     private void Update()
@@ -28,6 +41,14 @@ public class Player_Controller : MonoBehaviour
         PlayerMovement();
         Boundaries();
         Gun();
+        Sound();
+        DustParticlePlays();
+        Landed();
+
+        if (onFloor == false)
+        {
+            timeUp += 1;
+        }
     }
 
     private void Boundaries()
@@ -43,39 +64,115 @@ public class Player_Controller : MonoBehaviour
         }
     }
 
+    private void Sound()
+    {
+        if (feedbackActivation.soundOn == true)
+        {
+            if (isMoving == true && onFloor == true)
+            {
+                if (Time.time - timeBetweenFootsteps >= TimeFootstep)
+                {
+                    footsteps.PlayOneShot(footstep);
+                    timeBetweenFootsteps = Time.time;
+                }
+            }
+            else
+            {
+                footsteps.Pause();
+            }
+        }
+        else
+        {
+
+        }
+    }
+
     private void PlayerMovement()
     {
         if (Input.GetKey(KeyCode.D))
         {
             player.transform.position += Vector3.right * playerSpeed * Time.deltaTime;
-        }
+            isMoving = true;
+            player.transform.localScale = new Vector3(1, 1, 1);
 
-        if (Input.GetKey(KeyCode.A))
+            if (onFloor == true)
+            {
+                PlayerAnimator.SetInteger("Movement", 1);
+            }
+            else if (onFloor == false)
+            {
+                PlayerAnimator.SetInteger("Movement", 0);
+            }
+        }
+        else if (Input.GetKey(KeyCode.A))
         {
             player.transform.position += Vector3.left * playerSpeed * Time.deltaTime;
+            isMoving = true;
+            player.transform.localScale = new Vector3(-1, 1, 1);
+
+            if (onFloor == true)
+            {
+                PlayerAnimator.SetInteger("Movement", 1);
+            }
+            else if (onFloor == false)
+            {
+                PlayerAnimator.SetInteger("Movement", 0);
+            }
         }
+        else
+        {
+            isMoving = false;
+            PlayerAnimator.SetInteger("Movement", 0);
+        }
+
+        
 
         if (Input.GetKeyDown(KeyCode.Space) && onFloor == true)
         {
             playerRB.AddForce(Vector2.up * playerJumpSpeed, ForceMode2D.Impulse);
+            PlayerAnimator.SetInteger("Movement", 2);
         }
     }
 
     private void Gun()
     {
-        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0f;
-
-        Debug.Log(mousePosition);
-
-        if (mousePosition.x > player.transform.position.x)
+        if (Input.GetKey(KeyCode.D))
         {
-            gun.transform.rotation = Quaternion.Euler(0, 0, 0);
+            gun.transform.localScale = new Vector3(1, 0.1f, 1);
         }
 
-        if (mousePosition.x < player.transform.position.x)
+        if (Input.GetKey(KeyCode.A))
         {
-            gun.transform.rotation = Quaternion.Euler(0, 0, 180);
+            gun.transform.localScale = new Vector3(-1, 0.1f, 1);
+        }
+    }
+
+    private void DustParticlePlays()
+    {
+        if (Input.GetKeyDown(KeyCode.D) && onFloor == true)
+        {
+            dustParticles.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && onFloor == true)
+        {
+            dustParticles.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && onFloor == true)
+        {
+            dustParticles.Play();
+        }
+    }
+
+    private void Landed()
+    {
+        if (timeUp > 0)
+        {
+            if (onFloor)
+            {
+                Debug.Log("Landed");
+                timeUp = 0;
+                dustParticlesLand.Play();
+            }
         }
     }
 }
